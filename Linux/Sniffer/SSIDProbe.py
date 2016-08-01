13109b0198cdd1642d10aa2e0949a6d0eac2e5dd
zhangzfmail <zhangzfmail@gmail.com>
2016-08-01 11:05:35 -0400

    Linux Penetration Testing Tools

---
+++ Linux/Sniffer/SSIDProbe.py
@@ -0,0 +1,17 @@
+##############################################################
+#                    SSID Probing Based on Scapy             #
+##############################################################
+from scapy.all import *
+interface ='wlan0'
+probe_req = []
+ap_name = raw_input("Please enter the AP name: ")
+def probesniff(fm):
+    if fm.haslayer(Dot11ProbeReq):
+        client_name = fm.info
+        if client_name == ap_name :
+            if fm.addr2 not in probe_req:
+                print "New Probe Request: ", client_name 
+                print "MAC: ", fm.addr2
+                probe_req.append(fm.addr2)
+			
+sniff(iface= interface,prn=probesniff)
