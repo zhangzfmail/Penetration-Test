c47f9ad0c7e55aba3d7165c7b3e529956c0a7518
zhangzfmail <zhangzfmail@gmail.com>
2016-08-01 11:19:47 -0400

    Linux Penetration Testing Tools

---
+++ Linux/Detecting/DeauthDetector.py
@@ -0,0 +1,14 @@
+##############################################################
+#      Wireless Deauthentication Detector Based on Scapy     #
+##############################################################
+from scapy.all import *
+interface = 'wlan0'
+i=1
+def info(fm):
+    if fm.haslayer(Dot11):
+        if ((fm.type == 0) & (fm.subtype==12)):
+            global i
+            print "Deauth detected --> ", i
+            i=i+1
+						
+sniff(iface=interface,prn=info)
